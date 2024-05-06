eaf18cc1aa295f169db2add00d1a5711f1685758
lisa2305 <lisa.giordani@marconirovereto.it>
2024-05-06 08:01:47 +0200

    creazione progetto

---
+++ src/main/java/it/marconi/verifica_giordani/VerificaGiordaniApplication.java
@@ -0,0 +1,13 @@
+package it.marconi.verifica_giordani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerificaGiordaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerificaGiordaniApplication.class, args);
+	}
+
+}
